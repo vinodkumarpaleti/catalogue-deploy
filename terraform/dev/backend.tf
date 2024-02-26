09ad41527d79baf226e38a905e10699114bbdf77
vinodkumar <kumarvinod.paleti@gmail.com>
2024-02-26 10:59:57 +0530

    new repo added

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,4 +1,4 @@
-bucket   = "roboshop-remote-state"
+bucket   = "roboshop-remote-state-pvk"
 key = "catalogue-dev"
 region = "us-east-1"
 dynamodb_table = "roboshop-locking"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,7 +16,7 @@ variable "common_tags" {
 }
 
 variable "domain_name" {
-  default = "joindevops.online"
+  default = "jcglobalit.online"
 }
 
 variable "app_version" {
